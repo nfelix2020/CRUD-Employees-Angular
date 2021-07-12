d3f74edbfad3619bd178b5ffde1ba29a54a4137e
Félix <nfelix2020@yahoo.com>
2021-07-12 16:49:13 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
+import { EditComponent } from './employees/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
